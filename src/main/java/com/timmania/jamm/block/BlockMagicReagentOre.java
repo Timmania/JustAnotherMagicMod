ef824e03b1d15b101f93a4f02311ee0594d83995
Timmania <timmania@solcon.nl>
2019-06-14 16:07:03 +0200

    added generic mod ore

--- src/main/java/com/timmania/jamm/block/BlockMagicReagentOre.java
+++ src/main/java/com/timmania/jamm/block/BlockMagicReagentOre.java
@@ -1,10 +1,16 @@
 package com.timmania.jamm.block;
 
 import com.timmania.jamm.Jamm;
+import com.timmania.jamm.init.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+import java.util.Random;
 
 public class BlockMagicReagentOre extends Block {
+    Item toDrop;
 
     public BlockMagicReagentOre(String name) {
         super(Material.ROCK);
@@ -13,5 +19,15 @@ public class BlockMagicReagentOre extends Block {
         this.setRegistryName(name);
         this.setCreativeTab(Jamm.JAMM_TAB);
 
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune){
+        return ModItems.MAGIC_REAGENT;
+    }
+
+    @Override
+    public int quantityDropped(Random random){
+        return 4 + random.nextInt(2);
     }
 }
--- src/main/java/com/timmania/jamm/item/ItemMagicShovel.java
+++ src/main/java/com/timmania/jamm/item/ItemMagicShovel.java
@@ -1,6 +1,7 @@
 package com.timmania.jamm.item;
 
 import com.timmania.jamm.Jamm;
+
 import net.minecraft.item.ItemSpade;
 
 public class ItemMagicShovel extends ItemSpade {
